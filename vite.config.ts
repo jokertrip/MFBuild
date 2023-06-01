370766459d3ed3a934f6b5da1936bd7ac88322e3
jokerTrip <msimashkin@gmail.com>
2023-06-01 15:16:35 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import sass from 'sass'
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    host: 'localhost'
+  }
+})
